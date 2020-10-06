0124374bd46208c2bb0d3581ac0418a134d0055f
ChandraWijaya <42259235+ChandraW556@users.noreply.github.com>
2020-10-06 14:54:08 +0700

    modifikasi cek login

--- Pertemuan-4/ceklogin.php
+++ Pertemuan-4/ceklogin.php
@@ -1,17 +1,22 @@
 <?php
+session_start();
 
-if (isset($_POST['tombolsubmit'])){
+if (isset($_POST['tombolSubmit'])) {
     $username = $_POST['username'];
     $password = $_POST['password'];
 
-    if($username == "anto" && $password == "123") {
-        echo "Sukses";
+    if ($username == "anto" && $password == "123") {
+        //echo "Sukses";
+        $_SESSION["sessionUsername"] = $username;
+        header("Location: dashboard.php");
     }
     else {
-        echo "Username / Password ada yang salah ";
+        echo "Username / Password ada yg salah";
     }
 }
 else {
-    echo " Mohon maaf cek login tidak bisa di akses langsung ";
+    header("Location: login.php");
 }
+
+
 ?>
