a93d690fc23fe142975ebbb2dddafab4f4316652
ChandraWijaya <42259235+ChandraW556@users.noreply.github.com>
2020-10-23 14:45:35 +0700

    Update Firur edit pada edit_mk2.php
    menambahkan button simpan dan cancel pada edit_mk2.php

--- Pertemuan-6/edit_mk2.php
+++ Pertemuan-6/edit_mk2.php
@@ -1,67 +1,45 @@
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Registrasi Mata Kuliah</title>
-    <!-- CSS only -->
+    <title>Edit Mata Kuliah</title>
     <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css" integrity="sha384-JcKb8q3iqJ61gNV9KGb8thSsNjpSL0n8PARn9HuZOnIxN0hoP+VmmDGMN5t9UJ0Z" crossorigin="anonymous">
-    
-</head>
-<body>
-    <?php   
-    include_once "header.php";
-    include_once "koneksi.php"; 
-    $status = 2;  
-    if (isset($_POST['kodemk'])) {
-        
-        $kodemk = $_POST['kodemk'];
-        $namamk = $_POST['namamk'];
-        $kategori = $_POST['kategori'];
-        $sks = $_POST['sks']; 
 
-         //buat koneksi
-         $strsql = "INSERT INTO matakuliah (kodemk, namamk, kategori, sks) 
-         VALUES ('$kodemk','$namamk','$kategori','$sks')";
-         
-         $runSQL = mysqli_query($conn,$strsql);        
-         if ($runSQL) {
-             $status = 1; //sukses
-         }  
-         else {
-             $status = 0; //tidak sukses;
-         }       
-    }        
-    else if (isset($_GET['kodemk'])) {
-        $_kodemk = $_GET['kodemk'];
-        $strSQL = "SELECT * FROM matakuliah WHERE kodemk='".$_kodemk."'";
-        $runStrSQL = mysqli_query($conn,$strSQL);
-        $jmlRowData = mysqli_num_rows($runStrSQL);
-        if ($jmlRowData > 0) {
-            while ($row = mysqli_fetch_assoc($runStrSQL)) {
-                $_namamk = $row["namamk"];
-                $_kategori = $row["kategori"];
-                $_sks = $row["sks"];
-            }
-        }
-    }  
-    else {
-        $jngiseng = "disabled";
-        $_namamk = "";
-        $_kategori = "";
-        $_sks = "";
-        $_kodemk = "";
-    }  
+
+</head>
+
+<body>
+    <?php
+    include_once "header.php";
+    include_once "koneksi.php";
+    if (!isset($_GET['id'])) {
+        header('Location: listmatakuliah.php');
+    }
+
+    //ambil id dari query string
+    $id = $_GET['id'];
+
+    // buat query untuk ambil data dari database
+    $strsql = "SELECT * FROM matakuliah WHERE id=$id";
+    $runSQL = mysqli_query($conn, $strsql);
+    $row = mysqli_fetch_assoc($runSQL);
+
+    // jika data yang di-edit tidak ditemukan
+    if (mysqli_num_rows($runSQL) < 1) {
+        die("data tidak ditemukan...");
+    }
     ?>
     <div class="container">
-        <h2>Pendaftaran Mata Kuliah versi 2 (dg Modal)</h2>   
-    <!-- Ini Modal -->
+        <h2 class="text-center">Pengeditan Mata Kuliah versi 2 (dg Modal)</h2>
+        <!-- Ini Modal -->
         <div class="modal" id="pesan">
             <div class="modal-dialog">
                 <div class="modal-content">
                     <!-- ini header -->
                     <div class="modal-header">
-                        <h4 class="modal-title">Konfirmasi Pendaftaran</h4>
+                        <h4 class="modal-title">Konfirmasi Pengeditan</h4>
                         <button type="button" class="close" data-dismiss="modal">&times;</button>
                     </div>
 
@@ -96,53 +74,52 @@
             </div>
         </div>
 
-    <!-- ini end modal -->
-        <?php 
-            if ($status == 1) {
-        ?>    
-            <div class="alert alert-success alert-dismissible fade show">
-                <button type="button" class="close" data-dismiss="alert">&times;</button>
-                Data berhasil diinput ke dalam database.
-            </div>
-        <?php 
-            }
-            else if ($status == 0){
-        ?>
-            <div class="alert alert-danger alert-dismissible fade show">
-                <button type="button" class="close" data-dismiss="alert">&times;</button>
-                Data tidak berhasil diinput ke dalam database.
-            </div>
-        <?php 
-            }
-        
-        ?>
-        <form id="myform" method="post" action="registrasi_mk2.php">
-            <div class="form-group">
-                <label>Kode Mata Kuliah</label>
-                <input id="kodemk" class="form-control" type="text" name="kodemk" value="<?php echo $_kodemk ?>" readonly <?php echo $jngiseng ?>>
-            </div>
-            <div class="form-group">
-                <label>Nama Mata Kuliah</label>
-                <input id="namamk" class="form-control" type="text" name="namamk" value="<?php echo $_namamk ?>" <?php echo $jngiseng ?>>
-            </div>
-            <div class="form-group">
-                <label>Kategori Mata Kuliah</label>
-                <select id="kategori" name="kategori" class="form-control" <?php echo $jngiseng ?>>
-                <option value="pilih">Pilih</option>
-                <option value="MKMA">Mata Kuliah Major</option>
-                <option value="MKMI">Mata Kuliah Minor</option>
-                <option value="MKPIL">Mata Kuliah Pilihan</option>
-                </select>
-            </div>
-            <div class="form-group">
-                <label>SKS</label>
-                <input id="sks" class="form-control" type="text" name="sks" value="<?php echo $_sks ?>" <?php echo $jngiseng ?>>
-            </div>           
-                <input class="btn btn-primary" type="button" id="tombol" value="Simpan" <?php echo $jngiseng ?>>   
-        </form>
-        
+        <!-- ini end modal -->
+
+        <div class="container">
+
+            <form id="myform" method="POST" action="proses_edit.php">
+
+                <fieldset>
+
+                    <input type="hidden" name="id" value="<?php echo $row['id'] ?>" />
+
+                    <div class="form-group">
+                        <label for="kodemk">Kode Mata Kuliah</label>
+                        <input type="text" id="kodemk" name="kodemk" class="form-control" placeholder="Kode Mata Kuliah" value="<?php echo $row['kodemk'] ?>">
+                    </div>
+                    <div class="form-group">
+                        <label for="namamk">Nama Mata Kuliah</label>
+                        <input type="text" id="namamk" name="namamk" class="form-control" placeholder="Nama Mata Kuliah" value="<?php echo $row['namamk'] ?>">
+                    </div>
+                    <div class="form-group">
+                        <label for="kategori">Kategori Mata Kuliah</label>
+                        <?php $kategori = $row['kategori']; ?>
+                        <select id="kategori" name="kategori" class="form-control">
+                            <option value="MKMA" <?php echo ($kategori == 'MKMA') ? "selected" : "" ?>>Mata Kuliah Major</option>
+                            <option value="MKMI" <?php echo ($kategori == 'MKMI') ? "selected" : "" ?>>Mata Kuliah Minor</option>
+                            <option value="MKPIL" <?php echo ($kategori == 'MKPIL') ? "selected" : "" ?>>Mata Kuliah Pilihan</option>
+                        </select>
+                    </div>
+                    <div class="form-group">
+                        <label for="sks">SKS</label>
+                        <input id="sks" type="number" name="sks" class="form-control" placeholder="Satuan Kredit Semester" value="<?php echo $row['sks'] ?>">
+                    </div>
+                    <button type="button" id="tombol" class="btn btn-success btn-block"> Save
+                    </button>
+                    </input>
+                    <br>
+                    <a class="btn btn-danger btn-block" href="listmatakuliah.php"> Cancel</a>
+                    <br>
+                </fieldset>
+
+
+            </form>
+        </diV>
+
+
     </div>
-    <?php 
+    <?php
     include_once "footer.php";
     ?>
     <!-- JS, Popper.js, and jQuery -->
@@ -150,29 +127,29 @@
     <script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.1/dist/umd/popper.min.js" integrity="sha384-9/reFTGAW83EW2RDu2S0VKaIzap3H66lZH81PoYlFhbGU+6BZp6G7niu735Sk7lN" crossorigin="anonymous"></script>
     <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/js/bootstrap.min.js" integrity="sha384-B4gt1jrGC7Jh4AgTPSdUtOBvfO8shuf57BaghqFfPlYxofvL8/KUEfYiJOMMV+rV" crossorigin="anonymous"></script>
     <script>
-    $(document).ready(function() {
-        $('#proses').click(function(){
-            $('#myform').submit();
-        });
-        $('#tombol').click(function(){
-            //ambil data dari form
-            const kodemk = $('#kodemk').val();
-            const namamk = $('#namamk').val();
-            const kategori = $('#kategori').val();
-            const sks = $('#sks').val();
+        $(document).ready(function() {
+            $('#proses').click(function() {
+                $('#myform').submit();
+            });
+            $('#tombol').click(function() {
+                //ambil data dari form
+                const kodemk = $('#kodemk').val();
+                const namamk = $('#namamk').val();
+                const kategori = $('#kategori').val();
+                const sks = $('#sks').val();
 
-            $('#kdmk').text(kodemk);
-            $('#nmmk').text(namamk);
-            $('#kat').text(kategori);
-            $('#sksmk').text(sks);
-         
-            //buka modal
-            $('#pesan').modal({
-                show: true
+                $('#kdmk').text(kodemk);
+                $('#nmmk').text(namamk);
+                $('#kat').text(kategori);
+                $('#sksmk').text(sks);
+
+                //buka modal
+                $('#pesan').modal({
+                    show: true
+                });
             });
         });
-    });
-    
     </script>
 </body>
+
 </html>
